f251006e148b2fa756f403bb4632c7ca1547c3f0
celsojr-websters <celso.j@websters.ae>
2026-01-05 12:39:26 +0400

    Update Admin Client test

--- tests/PocketBase.Blazor.IntegrationTests/Clients/Admin/AuthWithPasswordTests.cs
+++ tests/PocketBase.Blazor.IntegrationTests/Clients/Admin/AuthWithPasswordTests.cs
@@ -1,21 +1,21 @@
 namespace PocketBase.Blazor.IntegrationTests.Clients.Admin;
 
-[Collection("PocketBase.Blazor Clients")]
+[Collection("PocketBase.Blazor.Admin")]
 public class AuthWithPasswordTests
 {
-    private readonly PocketBaseUserFixture _fixture;
+    private readonly IPocketBase _pb;
+    private readonly PocketBaseAdminFixture _fixture;
 
-    public AuthWithPasswordTests(PocketBaseUserFixture fixture)
+    public AuthWithPasswordTests(PocketBaseAdminFixture fixture)
     {
         _fixture = fixture;
+        _pb = fixture.Client;
     }
 
     [Fact]
     public async Task Auth_with_valid_credentials_returns_token()
     {
-        var pb = new PocketBase(_fixture.Settings.BaseUrl);
-
-        var result = await pb.Admins
+        var result = await _pb.Admins
             .AuthWithPasswordAsync(
                 _fixture.Settings.UserTesterEmail,
                 _fixture.Settings.UserTesterPassword
