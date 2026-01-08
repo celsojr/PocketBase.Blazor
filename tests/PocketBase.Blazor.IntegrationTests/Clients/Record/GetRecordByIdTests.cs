cd48dd40ccd94208db91af111d9636705c99adb8
celsojr-websters <celso.j@websters.ae>
2026-01-08 16:51:37 +0400

    Get Record by id test added

--- tests/PocketBase.Blazor.IntegrationTests/Clients/Record/GetRecordByIdTests.cs
+++ tests/PocketBase.Blazor.IntegrationTests/Clients/Record/GetRecordByIdTests.cs
@@ -1,5 +1,40 @@
 namespace PocketBase.Blazor.IntegrationTests.Clients.Record;
 
-internal class GetRecordByIdTests
+using Blazor.Responses;
+
+[Collection("PocketBase.Blazor.User")]
+public class GetRecordByIdTests
 {
+    private readonly IPocketBase _pb;
+
+    public GetRecordByIdTests(PocketBaseUserFixture fixture)
+    {
+        _pb = fixture.Client;
+    }
+
+    [Fact]
+    public async Task Get_one_record_by_id()
+    {
+        var listResult = await _pb.Collection("users")
+            .GetListAsync<RecordResponse>(perPage: 1);
+
+        listResult.IsSuccess.Should().BeTrue();
+        var recordId = listResult.Value.Items.First().Id;
+
+        var getResult = await _pb.Collection("users")
+            .GetOneAsync<RecordResponse>(recordId);
+
+        getResult.IsSuccess.Should().BeTrue();
+        getResult.Value.Id.Should().Be(recordId);
+    }
+
+    [Fact]
+    public async Task Get_one_record_with_invalid_id_returns_error()
+    {
+        var result = await _pb.Collection("users")
+            .GetOneAsync<RecordResponse>("invalid-id-that-doesnt-exist");
+
+        result.IsSuccess.Should().BeFalse();
+    }
 }
+
