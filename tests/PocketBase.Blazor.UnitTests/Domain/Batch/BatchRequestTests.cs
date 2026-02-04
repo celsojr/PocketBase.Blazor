30a783230ad9945ae7335ba21e7c97d6dd5750d6
celsojr-websters <celso.j@websters.ae>
2026-02-04 11:39:59 +0400

    Add missing file

--- tests/PocketBase.Blazor.UnitTests/Domain/Batch/BatchRequestTests.cs
+++ tests/PocketBase.Blazor.UnitTests/Domain/Batch/BatchRequestTests.cs
@@ -1,62 +1,95 @@
 namespace PocketBase.Blazor.UnitTests.Domain.Batch;
 
+using System;
 using Blazor.Enums;
-using Blazor.Requests;
+using Blazor.Requests.Batch;
 using FluentAssertions;
+using Xunit;
 
 public class BatchRequestTests
 {
     [Fact]
-    public void BatchRequest_Constructor_ShouldSetPropertiesCorrectly()
+    public void Constructor_Create_ShouldSetMethodUrlAndJson()
     {
         // Arrange & Act
-        var request = new BatchRequest("test-collection", BatchMethod.Create, new { name = "test" });
+        var request = new BatchRequest(
+            collectionName: "test-collection",
+            method: BatchMethod.Create,
+            body: new { name = "test" }
+        );
 
         // Assert
         request.Method.Should().Be("POST");
-        request.Url.Should().Be("api/collections/test-collection/records");
-        request.Json.Should().NotBeNull();
-        request.Json!["data"].GetProperty("name").GetString().Should().Be("test");
+        request.Url.Should().Be("/api/collections/test-collection/records");
+
+        request.Body.Should().NotBeNull();
+        request.Body!.Value.TryGetProperty("name", out var name).Should().BeTrue();
+        name.Should().NotBeNull();
+        name.GetString().Should().Be("test");
     }
 
     [Fact]
-    public void BatchRequest_Constructor_WithUpdate_ShouldIncludeId()
+    public void Constructor_Update_ShouldSetMethodUrlAndJson()
     {
         // Arrange & Act
-        var request = new BatchRequest("test-collection", BatchMethod.Update, new { name = "updated" }, "record-123");
+        var request = new BatchRequest(
+            collectionName: "test-collection",
+            method: BatchMethod.Update,
+            body: new { name = "updated" },
+            id: "record-123"
+        );
 
         // Assert
         request.Method.Should().Be("PATCH");
-        request.Url.Should().Be("api/collections/test-collection/records/record-123");
+        request.Url.Should().Be("/api/collections/test-collection/records/record-123");
+
+        request.Body.Should().NotBeNull();
+
+        request.Body!.Value.TryGetProperty("name", out var name).Should().BeTrue();
+        name.Should().NotBeNull();
+        name.GetString().Should().Be("updated");
     }
 
     [Fact]
-    public void BatchRequest_Constructor_WithDelete_ShouldNotRequireBody()
+    public void Constructor_Delete_ShouldSetMethodAndUrl_AndHaveNoJson()
     {
         // Arrange & Act
-        var request = new BatchRequest("test-collection", BatchMethod.Delete, null, "record-123");
+        var request = new BatchRequest(
+            collectionName: "test-collection",
+            method: BatchMethod.Delete,
+            body: null,
+            id: "record-123"
+        );
 
         // Assert
         request.Method.Should().Be("DELETE");
-        request.Url.Should().Be("api/collections/test-collection/records/record-123");
-        request.Json.Should().BeNull();
+        request.Url.Should().Be("/api/collections/test-collection/records/record-123");
+        request.Body.Should().BeNull();
     }
 
     [Fact]
-    public void BatchRequest_Constructor_WithoutIdForUpdate_ShouldThrow()
+    public void Constructor_Update_WithoutId_ShouldThrow()
     {
         // Act
-        var act = () => new BatchRequest("test-collection", BatchMethod.Update, new { });
+        Action act = () => new BatchRequest(
+            collectionName: "test-collection",
+            method: BatchMethod.Update,
+            body: new { name = "fail" }
+        );
 
         // Assert
         act.Should().Throw<ArgumentNullException>();
     }
 
     [Fact]
-    public void BatchRequest_Constructor_WithoutIdForDelete_ShouldThrow()
+    public void Constructor_Delete_WithoutId_ShouldThrow()
     {
         // Act
-        var act = () => new BatchRequest("test-collection", BatchMethod.Delete, null);
+        Action act = () => new BatchRequest(
+            collectionName: "test-collection",
+            method: BatchMethod.Delete,
+            body: null
+        );
 
         // Assert
         act.Should().Throw<ArgumentNullException>();
