4bdd5a9d556d66e08c74d6765341d9a86589269c
celsojr-websters <celso.j@websters.ae>
2026-02-11 18:58:59 +0400

    New test user added

--- tests/PocketBase.Blazor.IntegrationTests/Data/pb_migrations/1687801090_initial_regular_user.js
+++ tests/PocketBase.Blazor.IntegrationTests/Data/pb_migrations/1687801090_initial_regular_user.js
@@ -4,10 +4,13 @@ migrate((app) => {
     let users = app.findCollectionByNameOrId("users")
 
     let record = new Record(users)
-
     record.set("email", "user_tester@email.com")
     record.set("password", "Nyp9wiGaAC4qGWz")
-
     app.save(record)
+
+    let newRecord = new Record(users)
+    newRecord.set("email", "user_tester2@email.com")
+    newRecord.set("password", "Xk7mL9pQrS2tUvW")
+    app.save(newRecord)
 })
 
